41e5a024fea240e5bc1bade5501e6607b98f633e
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-05-24 13:57:59 +0200

    Bug fixes

--- Tuning.R
+++ Tuning.R
@@ -34,18 +34,20 @@ saveRDS(rf_tuned, "./saved_models/rf_tuned.rds")
 ######################################################################################
 ######################################################################################
 
-tg_XG1 <- data.table(expand.grid(nrounds = seq(from = 200, to = nrounds, by = 50),
-                               nrounds = seq(from = 200, to = nrounds, by = 50),
+# Tuning Round 1
+nrounds <- 500
+tg_XG1 <- expand.grid(nrounds = seq(from = 200, to = nrounds, by = 50),
                                eta = c(0.025, 0.05, 0.1, 0.3),
                                max_depth = c(2, 3, 4, 5, 6),
                                gamma = 0,
                                colsample_bytree = 1,
                                min_child_weight = 1,
-                               subsample = 1))
+                               subsample = 1)
 
 xg_tuned1 <- pipeline_casero(bank_train, model="xgbTree", tunegrid=tg_XG1)
 
-tg_XG2 <- data.table(expand.grid(nrounds = seq(from = 50, to = nrounds, by = 50),
+# Tuning Round 2
+tg_XG2 <- expand.grid(nrounds = seq(from = 50, to = nrounds, by = 50),
                                eta = xg_tuned1$bestTune$eta,
                                max_depth = ifelse(xg_tuned1$bestTune$max_depth == 2,
                                                   c(xg_tuned1$bestTune$max_depth:4),
@@ -53,47 +55,51 @@ tg_XG2 <- data.table(expand.grid(nrounds = seq(from = 50, to = nrounds, by = 50)
                                gamma = 0,
                                colsample_bytree = 1,
                                min_child_weight = c(1, 2, 3),
-                               subsample = 1))
+                               subsample = 1)
 
 xg_tuned2 <- pipeline_casero(bank_train, model="xgbTree", tunegrid=tg_XG2)
 
-tg_XG3 <- data.table(expand.grid(nrounds = seq(from = 50, to = nrounds, by = 50),
+# Tuning Round 3
+tg_XG3 <- expand.grid(nrounds = seq(from = 50, to = nrounds, by = 50),
                                eta = xg_tuned1$bestTune$eta,
                                max_depth = xg_tuned2$bestTune$max_depth,
                                gamma = 0,
                                colsample_bytree = c(0.4, 0.6, 0.8, 1.0),
                                min_child_weight = xg_tuned2$bestTune$min_child_weight,
-                               subsample = c(0.5, 0.75, 1.0)))
+                               subsample = c(0.5, 0.75, 1.0))
                    
 xg_tuned3 <- pipeline_casero(bank_train, model="xgbTree", tunegrid=tg_XG3)
 
-tg_XG4 <- data.table(expand.grid(nrounds = seq(from = 50, to = nrounds, by = 50),
+# Tuning Round 4
+tg_XG4 <- expand.grid(nrounds = seq(from = 50, to = nrounds, by = 50),
                                 eta = xg_tuned1$bestTune$eta,
                                 max_depth = xg_tuned2$bestTune$max_depth,
                                 gamma = c(0, 0.05, 0.1, 0.5, 0.7, 0.9, 1.0),
                                 colsample_bytree = xg_tuned3$bestTune$colsample_bytree,
                                 min_child_weight = xg_tuned2$bestTune$min_child_weight,
-                                subsample = xg_tuned3$bestTune$subsample))
+                                subsample = xg_tuned3$bestTune$subsample)
 
 xg_tuned4 <- pipeline_casero(bank_train, model="xgbTree", tunegrid=tg_XG4)
 
-tg_XG5 <- data.table(expand.grid(nrounds = seq(from = 100, to = 10000, by = 100),
+# Tuning Round 5
+tg_XG5 <- expand.grid(nrounds = seq(from = 100, to = 10000, by = 100),
                                 eta = c(0.01, 0.015, 0.025, 0.05, 0.1),
                                 max_depth = xg_tuned2$bestTune$max_depth,
                                 gamma = xg_tuned4$bestTune$gamma,
                                 colsample_bytree = xg_tuned3$bestTune$colsample_bytree,
                                 min_child_weight = xg_tuned2$bestTune$min_child_weight,
-                                subsample = xg_tuned3$bestTune$subsample))
+                                subsample = xg_tuned3$bestTune$subsample)
 
 xg_tuned5 <- pipeline_casero(bank_train, model="xgbTree", tunegrid=tg_XG5)
 
-final_grid <- data.table(expand.grid(nrounds = tg_XG5$bestTune$nrounds,
+# Final Tuning Round
+final_grid <- expand.grid(nrounds = tg_XG5$bestTune$nrounds,
                                     eta = tg_XG5$bestTune$eta,
                                     max_depth = tg_XG5$bestTune$max_depth,
                                     gamma = tg_XG5$bestTune$gamma,
                                     colsample_bytree = tg_XG5$bestTune$colsample_bytree,
                                     min_child_weight = tg_XG5$bestTune$min_child_weight,
-                                    subsample = tg_XG5$bestTune$subsample))
+                                    subsample = tg_XG5$bestTune$subsample)
 
 xg_final <- pipeline_casero(bank_train, model="xgbTree", tunegrid=final_grid)
 
