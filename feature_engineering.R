c3e5de98a2d86cd25d899fe919236390832d5e59
tomastello <44135714+tomastello@users.noreply.github.com>
2019-06-03 14:23:29 +0200

    Create feature_engineering.R

---
+++ feature_engineering.R
@@ -0,0 +1,57 @@
+drop_variables <- function(df){
+  df_sub <- df[, -c('duration', 'pdays', 'month', 'day')]
+  
+  return(df_sub)
+}
+
+create_weekday <- function(df){
+  df1 <- data.table(df)
+  
+  df1$month <- factor(df1$month, levels = c("jan", "feb", "mar", 
+                                            "apr", "may", "jun", 
+                                            "jul", "aug", "sep", 
+                                            "oct", "nov", "dec"))
+  
+  df1$month_lev <- as.integer(df1$month)
+  
+  
+  df1$date <- ISOdate(2014, df1$month_lev , df1$day)
+  
+  df1$weekday <- ISOweekday(df1$date)
+  
+  return(df1[,-c('month_lev', 'date')])
+  
+}
+
+
+total_contacts <- function(df){
+  df1 <- data.table(df)
+  
+  df1$total_contact <- df1$campaign + df1$previous
+  
+  return(df1[,-c('campaign', 'previous')])
+}
+
+clustering <- function(df_train, df_test, n_clusters){
+  
+  df_train$train <- 1
+  df_test$train <- 0
+  
+  df1 <- rbind(df_train, df_test, fill = TRUE)
+  
+  set.seed(1912)
+  clusters <- kmeans(scale(df1[,c("age", 'balance', 'total_contact')]),n_clusters)
+  
+  df1$cluster <- as.factor(clusters$cluster)
+  
+  df_train <- df1[df1$train == 1, -'train']
+  df_test <- df1[df1$train == 0, -'train']
+  
+  return(list("train" = df_train, "test" = df_test))
+  
+}
+
+
+
+
+
